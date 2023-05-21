31c3910519cc30cc7573cf365b4d2e16d395d085
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-05-21 11:24:51 +0300

    adding the querySelectors

---
+++ script.js
@@ -0,0 +1,5 @@
+//my variables with querySelector
+const myForm = document.querySelector('#my-form')
+const submitButton = document.querySelector('button')
+const itemInbut = document.querySelector('#item-input')
+
